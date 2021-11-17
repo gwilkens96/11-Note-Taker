b38035b98fa59d0873412bf6acb8990f240e767e
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-17 11:45:30 -0500

    setup storage and edited routing

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -13,7 +13,7 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", (req, res) => {
-  store
+  storage
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));

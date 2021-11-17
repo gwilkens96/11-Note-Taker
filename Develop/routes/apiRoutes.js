bd9aab73dc7b1e089984abc93e3f3274f71f7962
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-17 11:00:55 -0500

    built storage for notes and dl uuid pckge

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,12 +1,22 @@
-const path = require("path");
 const router = require("express").Router();
+const storage = require("../db/storage");
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  store
+    .getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
